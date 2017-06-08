9bab4fbc9e144ceb086fb0e41dc232408727eac7
Vaishakh Nair <vaishakhnair@Vaishakh.local>
2017-06-08 23:26:42 +0530

    removed target files

--- Practice/practice/src/main/java/com/nair/practice/datastructures/BinarySearchTreeDriver.java
+++ Practice/practice/src/main/java/com/nair/practice/datastructures/BinarySearchTreeDriver.java
@@ -1,7 +1,6 @@
 package com.nair.practice.datastructures;
 
 import com.nair.practice.datastructures.trees.BinarySearchTree;
-import com.nair.practice.datastructures.trees.BinarySearchTreeNode;
 
 public class BinarySearchTreeDriver {
 
@@ -10,9 +9,6 @@ public class BinarySearchTreeDriver {
 		int[] ints = new int[]{4,2,6,1,3,5,7};
 		for(int i : ints){
 			binarySearchTree.insert(i);
-		}
-		for(BinarySearchTreeNode<Integer> node : binarySearchTree.levelOrderTraversal()){
-			System.out.println(node.getData());
 		}
 	}
 }
